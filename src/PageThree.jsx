f1bfc98b8bde6f840c38dc1189fe8704c7e9932c
Your Name <your-email@example.com>
2026-02-14 11:35:37 +0100

    Update pages for priscilla with new changes

--- src/PageThree.jsx
+++ src/PageThree.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import tochiImg from "./assets/faith2.jpg";
-import tochiImg1 from "./assets/faith1.jpg";
+import tochiImg from "./assets/priscila1.jpg";
+import tochiImg1 from "./assets/priscila2.jpg";
 
 const PageThree = ({ onNext }) => {
   const photos = [1, 2];
